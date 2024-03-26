47caefb92af2fce80eb5db9131ef3d782e04c9e3
Kristian Leirimaa <krmapale@jyu.fi>
2024-03-26 17:46:06 +0200

    started writing code for adding components

--- tool/pqcbom.js
+++ tool/pqcbom.js
@@ -42,88 +42,25 @@ const args = yargs(hideBin(argv))
 
 
 
-for(const property in args){
-    console.log(`${property}: ${args[property]}`);
-
-}
-
-
 //TODO: do testing and make this a loop!
 if(args.i){
-    scanDirectory(args.i);
-    checkOutputFilename(args.o);
+    createBomFile(args.o, args.i);
 }
 if(args.git){
-    scanDirectory(args.git);
-    checkOutputFilename(args.o);
+    createBomFile(args.o, args.git);
 }
 if(args.docker){
-    scanDirectory(args.docker);
-    checkOutputFilename(args.o);
+    createBomFile(args.o, args.docker);
 }
 
-function checkOutputFilename(outputFilename){
-    if(outputFilename){
-        createBomFile(args.o);
-        console.log();
-    }
-    else {
-        createBomFile(undefined);
-        console.log();
-    }
-}
 
-/**
-const argvSpliced = argv.slice(2);
-const argvSplicedLength = argvSpliced.length;
 
-// Check for any arguments.
-if(argvSplicedLength > 0){
-    const optionIndex = 0;
-    const pathIndex = 1; 
-        // If a single argument is given and matches option for help or version, print values.
-        if(argvSplicedLength == 1){
-            if(argvSpliced[optionIndex] == '-h' || argvSpliced[optionIndex] == '--help'){
-                printManualPage();
-            }
-            if(argvSpliced[optionIndex] == '-v' || argvSpliced[optionIndex] == '--v'){
-                console.log(pqcbomVersion);
-            }
-        }
-        // If two arguments were given, check for correct options, followed by a valid directory path.
-        if(argvSplicedLength == 2){
-            if(argvSpliced == '--git'){
-                //TODO
-            }
-            if(argvSpliced == '--docker'){
-                //TODO
-            }
-            if(argvSpliced == '-i' || argvSpliced == '--i'){
-                //if(argv.length == 4)
-            }
-        }
-        else{
-            console.log("Not a valid argument. Use argument --h to see argument options.")
-        }
-}
-// If no arguments were given, scan current directory.
-else {
-    console.log("Scanning path: " + process.cwd());
-    scanDirectory(process.cwd());
-    createBomFile();
-}
-
-// Go through given arguments and save them to logCmdCall array. 
-argvSpliced.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
-    });
-*/
 
 /**
  * Scans files from given directory.
  * @param {Directory that will be scanned} directoryPath 
  */
-function scanDirectory(directoryPath) {
+function scanDirectory(directoryPath, bomObj) {
     fs.readdir(directoryPath, (err, files) => {
         if (err) {
             console.error('Error reading directory:', err);
@@ -143,20 +80,52 @@ function scanDirectory(directoryPath) {
                     scanDirectory(filePath); // Recursively scan subdirectory
                 } else {
                     console.log(filePath); // Log file path
-                    checkFileType(file, filePath);
+                    bomObj = addComponent(filePath, bomObj);
                 }
             });
         });
     });
 }
 
+function addComponent(filePath, bomObj){
+    const component = {
+        name: undefined,
+        type: 'cryptographic-asset',
+        bomref: undefined,
+        cryptoProperties: {
+            assetType: checkType(), //TODO: luo aliohjelma joka tarkistaa onko kyse algoritmistä, avaimesta, certistä...
+        }
+    }
 
+    switch (component.cryptoProperties.assetType){
+        case 'algorithm':
+            component.cryptoProperties.algorithmProperties = {
+                primitive: undefined, //TODO
+                parameterSetIdentifier: undefined, //TODO
+                mode: undefined,
+                executionEnvironment: undefined, //TODO
+                implemenetationPlatform: undefined, //TODO
+                certificationLevel: undefined,
+                cryptoFunctions: undefined, //TODO
+                classicalSecurityLevel: undefined, //TODO
+                nistQuantumSecurityLevel: undefined //TODO
+            }
+            break;
+        case 'certificate':
+            break;
+        case 'related-crypto-material':
+            break;
+        case 'protocol':
+            break;
+    }
+
+}
 
 /**
  * Create bom.json file. 
  * TODO: TEST
  */
-function createBomFile(filename){
+function createBomFile(filename, dirPath){
 
     //TODO: test if this works
     const fileNameExtension = '.json';
@@ -180,8 +149,11 @@ function createBomFile(filename){
                 name: "PQCBOM tool",
                 version: pqcbomVersion
             }
-        }
+        },
+        components: undefined
     }
+
+    scanDirectory(dirPath, bomObj);
     
     fs.writeFile(filename, JSON.stringify(bomObj, false, 2), (err) => {
         if (err) throw err;
