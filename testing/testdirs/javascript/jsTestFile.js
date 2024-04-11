ef04ca78725a985cb8f3f8b0ea34fa3cb5ee095b
Kristian Leirimaa <krmapale@jyu.fi>
2024-04-11 16:04:15 +0300

    created js test file

--- testing/testdirs/javascript/jsTestFile.js
+++ testing/testdirs/javascript/jsTestFile.js
@@ -5,6 +5,11 @@ import path from 'node:path';
 import { argv } from 'node:process';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import crypto from 'crypto';
+import * as crypto from 'crypto';
+import { createHash } from 'crypto';
+import { createHash as ch } from 'crypto';
+import crypto, { createHash } from 'crypto';
 
 
 
