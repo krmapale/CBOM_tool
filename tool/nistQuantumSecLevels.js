f67aaeec07cb99ab7a138b7218993e2d6882bd84
Kristian Leirimaa <krmapale@jyu.fi>
2024-04-21 14:16:17 +0300

    created a own file for getting nist quantum security levels

---
+++ tool/nistQuantumSecLevels.js
@@ -0,0 +1,82 @@
+
+export class NistQuantumSecLevel{
+    constructor(){
+        this.levelZero = {
+            level : 0,
+            algorithms : [ // very weak
+            // < AES 128, DES, SHA-224, RSA/DH-2048, ECC 224,
+            'DES',
+            'SHA-224',
+            'RSA-2048',
+            'DH-2048',
+            'ECC-224'
+            ]
+        }
+        this.levelOne = {
+            level : 1,
+            algorithms : [ // weak
+                'AES128',
+                'AES-128',
+                'KYBER-512',
+                'RSA-3072',
+                'DH-3072',
+                'ECC-256'        
+            ]
+        } 
+
+        this.levelTwo = {
+            level : 2, 
+            algorithms : [ // strong  
+                'SHA256',
+                'SHA3-256',
+            ]
+        }
+
+        this.levelThree = {
+            level : 3,
+            algorithms : [ // stonger
+                'AES192',
+                'AES-192',
+                'KYBER-768',
+                'RSA-7068',
+                'DH-7068',
+                'ECC-384'
+            ]
+        }
+
+        this.levelFour = {
+            level : 4,
+            algorithms : [ // very strong
+                'SHA384',
+                'SHA3-384'
+            ]
+        }
+
+        this.levelFive = {
+            level : 5,
+            algorithms : [ // strongest
+                'AES256',
+                'AES-256',
+                'KYBER-1024',
+                'ECC-512',
+                'SHA-512'
+            ]
+        } 
+        this.levelSix = {
+            level : 6,
+            algorithms : [ // ?
+                // ?
+            ]
+        } 
+    }
+}
+
+function getNistQuantumSecLevel(regexpMatchString) {
+        const nistQuantumSecLvlObject = new NistQuantumSecLevel();
+
+        nistQuantumSecLvlObject.forEach(propertyElement => {
+            propertyElement.algorithms.forEach(algorithm => {
+                
+            });
+        });
+}
